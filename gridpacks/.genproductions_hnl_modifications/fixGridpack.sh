95d2b255c5518d4f51b8e583cde20a3124bfe1df
tomc <tomc@cern.ch>
2020-01-16 10:13:15 +0100

    Updated sed for madgraph syntax error
    The sed statement for the madgraph output lhe syntax error was put in twice, only the second time is needed (the first one was introduced at some point by a central update of runcmsgrid.sh

--- gridpacks/.genproductions_hnl_modifications/fixGridpack.sh
+++ gridpacks/.genproductions_hnl_modifications/fixGridpack.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/$USER/temp_$gridpack
 cp $1 /tmp/$USER/temp_$gridpack/$gridpack
 cd /tmp/$USER/temp_$gridpack
 tar -xaf $gridpack
-sed -i 's#ls -l#ls -l\nsed -i "s/LesHouchesEvent>/LesHouchesEvents>/g" cmsgrid_final.lhe#g' runcmsgrid.sh
+sed -i 's#exit 0#sed -i "s/LesHouchesEvent>/LesHouchesEvents>/g" cmsgrid_final.lhe\nexit 0#g' runcmsgrid.sh
 tar cfJ $gridpack mgbasedir runcmsgrid.sh process gridpack_generation.log
 mv /tmp/$USER/temp_$gridpack/$gridpack $here
 rm -r /tmp/$USER/temp_$gridpack
