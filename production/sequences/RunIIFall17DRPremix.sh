7e56782da48a4d8d8a98c8825af28d480f9ede7c
tomc <tomc@cern.ch>
2020-09-24 14:19:07 +0200

    Remove files immediately after they became useless

--- production/sequences/RunIIFall17DRPremix.sh
+++ production/sequences/RunIIFall17DRPremix.sh
@@ -6,7 +6,7 @@ cmsswRelease CMSSW_9_4_7
 /user/$USER/production/proxyExpect.sh
 cmsDriver.py step1 --filein file:EXO-RunIIFall17wmLHEGS-heavyNeutrino.root --fileout file:EXO-RunIIFall17DR80Premix-heavyNeutrino_step1.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISummer17PrePremix-MCv2_correctPU_94X_mc2017_realistic_v9-v1/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 94X_mc2017_realistic_v11 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:2e34v40 --nThreads 8 --datamix PreMix --era Run2_2017 --python_filename EXO-RunIIFall17DR80Premix-heavyNeutrino_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $events || exit $? ;
 cmsRun -e -j EXO-RunIIFall17DR80Premix-heavyNeutrino_rt.xml EXO-RunIIFall17DR80Premix-heavyNeutrino_1_cfg.py || exit $? ;
+rm EXO-RunIIFall17wmLHEGS-heavyNeutrino.root
 cmsDriver.py step2 --filein file:EXO-RunIIFall17DR80Premix-heavyNeutrino_step1.root --fileout file:heavyNeutrinoAOD.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 94X_mc2017_realistic_v11 --step RAW2DIGI,RECO,EI --nThreads 8 --era Run2_2017 --python_filename EXO-RunIIFall17DR80Premix-heavyNeutrino_2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $events || exit $? ;
 cmsRun -e -j EXO-RunIIFall17DR80Premix-heavyNeutrino_2_rt.xml EXO-RunIIFall17DR80Premix-heavyNeutrino_2_cfg.py || exit $? ; 
-
-
+rm EXO-RunIIFall17DR80Premix-heavyNeutrino_step1.root
--- production/sequences/RunIISummer16DR80Premix.sh
+++ production/sequences/RunIISummer16DR80Premix.sh
@@ -6,5 +6,7 @@ cmsswRelease CMSSW_8_0_31
 /user/$USER/production/proxyExpect.sh
 cmsDriver.py step1 --filein file:EXO-RunIISummer15wmLHEGS-heavyNeutrino.root --fileout file:EXO-RunIISummer16DR80Premix-heavyNeutrino_step1.root  --pileup_input "dbs:/Neutrino_E-10_gun/RunIISpring15PrePremix-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v2-v2/GEN-SIM-DIGI-RAW" --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:@frozen2016 --nThreads 8 --datamix PreMix --era Run2_2016 --python_filename EXO-RunIISummer16DR80Premix-heavyNeutrino_1_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $events || exit $? ;
 cmsRun -e -j EXO-RunIISummer16DR80Premix-heavyNeutrino_rt.xml EXO-RunIISummer16DR80Premix-heavyNeutrino_1_cfg.py || exit $? ;
+rm EXO-RunIISummer15wmLHEGS-heavyNeutrino.root
 cmsDriver.py step2 --filein file:EXO-RunIISummer16DR80Premix-heavyNeutrino_step1.root --fileout file:heavyNeutrinoAOD.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step RAW2DIGI,RECO,EI --nThreads 4 --era Run2_2016 --python_filename EXO-RunIISummer16DR80Premix-heavyNeutrino_2_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $events || exit $? ;
 cmsRun -e -j EXO-RunIISummer16DR80Premix-heavyNeutrino_2_rt.xml EXO-RunIISummer16DR80Premix-heavyNeutrino_2_cfg.py || exit $? ;
+rm EXO-RunIISummer16DR80Premix-heavyNeutrino_step1.root
